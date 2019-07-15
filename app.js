db194425464df6f597603567840f96dc06fb73f8
jim2500 <shijun2828@163.com>
2019-07-15 15:04:56 +0800

    数据方法

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name: 'Shaun',
-        job: 'Ninja',
-        website: 'http://www.thenetninja.co.uk',
-        websiteTag: '<a href="http://www.thenetninja.co.uk">The Net Ninja Website</a>'
+        age: 25,
+        x: 0,
+        y: 0
     },
     methods: {
-        greet: function(time){
-            return 'Good ' + time + ', ' + this.name;
+        add: function(inc){
+            this.age += inc;
+        },
+        subtract: function(dec){
+            this.age -= dec;
+        },
+        updateXY: function(event){
+            this.x = event.offsetX;
+            this.y = event.offsetY;
         }
     }
 });
