84631884774ad3cd35a4642bcee77858b67a117f
jim2500 <shijun2828@163.com>
2019-07-15 14:53:30 +0800

    属性绑定

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
     el: '#vue-app',
     data: {
         name: 'Shaun',
-        job: 'Ninja'
+        job: 'Ninja',
+        website: 'http://www.thenetninja.co.uk',
+        websiteTag: '<a href="http://www.thenetninja.co.uk">The Net Ninja Website</a>'
     },
     methods: {
         greet: function(time){
