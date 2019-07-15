4556f0990af245be709c488e9bf1a38e291dbb61
jim2500 <shijun2828@163.com>
2019-07-15 15:00:49 +0800

    属性绑定

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 new Vue({
     el: '#vue-app',
     data: {
-        age: 25,
-        x: 0,
-        y: 0
+        name: 'Shaun',
+        job: 'Ninja',
+        website: 'http://www.thenetninja.co.uk',
+        websiteTag: '<a href="http://www.thenetninja.co.uk">The Net Ninja Website</a>'
     },
     methods: {
-        add: function(inc){
-            this.age += inc;
-        },
-        subtract: function(dec){
-            this.age -= dec;
-        },
-        updateXY: function(event){
-            this.x = event.offsetX;
-            this.y = event.offsetY;
+        greet: function(time){
+            return 'Good ' + time + ', ' + this.name;
         }
     }
 });
